261fee9410e0d128d0365c7baba441d762f430dd
SlavkoPar <slavko.parezanin@gmail.com>
2023-01-10 12:19:49 +0100

    icons

--- src/pages/Devtools/index.js
+++ src/pages/Devtools/index.js
@@ -1,5 +1,5 @@
 chrome.devtools.panels.create(
   'Dev Tools from chrome-extension-boilerplate-react',
-  'icon-34.png',
+  'support-34.png',
   'panel.html'
 );
--- webpack.config.js
+++ webpack.config.js
@@ -152,7 +152,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
+          from: 'src/assets/img/support-128.png',
           to: path.join(__dirname, 'build/images'),
           force: true,
         },
@@ -161,7 +161,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-34.png',
+          from: 'src/assets/img/support-34.png',
           to: path.join(__dirname, 'build/images'),
           force: true,
         },
