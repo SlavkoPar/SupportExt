72d450117d8054fd7cc33c09f8734897449f1156
SlavkoPar <slavko.parezanin@gmail.com>
2023-04-26 08:10:17 +0200

    send email

--- secrets.development.js
+++ secrets.development.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 module.exports.secrets = { 
-  myWebApp: 'http://localhost:3000/supporter',
+  myWebApp: 'https://support-knowledge.onrender.com/supporter',
+  mmmmmmmmmmmmmmmmmyWebApp: 'http://localhost:3000/supporter',
   mmmyWebApp: 'http://localhost:3001/Support/#/supporter',
   mmmmyWebApp: 'https://slavkopar.github.io/Support-Knowledge/#/supporter'
 };
